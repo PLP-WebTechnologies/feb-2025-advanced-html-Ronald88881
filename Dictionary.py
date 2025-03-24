b15dd87a21a9bf5a7c57512a3b5493802d7c4485
Ronald88881 <kinyeronald@gmail.com>
2025-03-24 16:58:51 +0300

    Resolved merge conflicts

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+capital_cities = {
+    "England": "London",
+    "Germany": "Berlin",
+    "France": "Paris",
+
+}
+
+print(capital_cities)
+print(capital_cities["England"])
+
---
+++ Tuply.py
@@ -0,0 +1,6 @@
+car =("Toyota", "Nissan", "Honda")
+print(car)
+print(car[0])
+print(car[1])
+print(car[2])
+
